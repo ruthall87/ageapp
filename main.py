5e25dd6eef49866d3d2da284e133b9b1bfea1d5e
Eleanor Hall <ruthhall97@gmail.com>
2022-01-04 12:18:10 -0800

    2 examples

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ user_age = input("Enter your age:")
 years = int(user_age)
 months = years * 12
 print(f"Your age, {years}, is equal to {months} months ")
+
+user_age = int(input("Enter your age:"))
+months = user_age * 12
+print(f"Your age, {years}, is equal to {months} months ")
