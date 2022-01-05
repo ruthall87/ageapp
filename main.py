f12f93b652f5272a287a31b7ec925cc5d35eb5d7
Eleanor Hall <ruthhall97@gmail.com>
2022-01-04 23:11:42 -0800

    html

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-user_age = input("Enter your age:")
-years = int(user_age)
-months = years * 12
-print(f"Your age, {years}, is equal to {months} months ")
+#not so clean code
+#user_age = input("Enter your age:")
+#years = int(user_age)
+#months = years * 12
+#print(f"Your age, {years}, is equal to {months} months ")
 
-user_age = int(input("Enter your age:"))
-months = user_age * 12
-print(f"Your age, {years}, is equal to {months} months ")
+
+
+# clean code 
+#user_age = int(input("Enter your age:"))
+
+#months = user_age * 12
+#print(f"Your age, {years}, is equal to {months} months ")
+
+# the key difference between list 
+# and tuples is that you can not modify a tuple 
+# YOU CAN ADD REMOVE ELEMENTS FROM A LIST YOU CAN NOT ADD AND REMOVE ELEMENTS FROM A TUPLE
+# IN A SET YOU CAN ADD AND REMOVE ELEMENTS BUT YOU CAN NOT HAVE DUPLICATES 
+# SETS DO NOT KEEP ORDER 
+# SETS ARE 
+#l = ['ELLY', 'ROLF', 'ANNE']
+#t = ('PETA-GAYE', 'ALEX','KELLY')
+#s = {'BOB', 'ROLF', 'ANNE'}
+
+#SUBSCRIPT NOTATION
+#print(l[0])
